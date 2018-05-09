f95895399f13b39ce3f55b86808dcc1a14599f76
UGURPC\UĞUR <boranu@mef.edu.tr>
2018-05-10 00:01:58 +0300

    no message

--- config/config/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    "localmongoDB": "mongodb://localhost/DavinciDB",
-    "remotemongoDB":"mongoRemoteUrl",
-    "secret": "danvincisecretkey",
-    "tokenExpireTime": "7 Days",
-    "thumbnailQuality":80,
-    "imageQuality":85
-}
--- config/signToken.js
+++
@@ -1,7 +0,0 @@
-var config = require('./config/config');
-var jwt = require('jsonwebtoken');
-
-module.exports = function(payload)
-{
-    return jwt.sign(payload, config.secret, { expiresIn: config.tokenExpireTime });
-}
